6bd8a5a9dd9d2bc6dc915c1a47ca71f447e74c27
Josh LaFayette <joshlafayette@gmail.com>
2020-08-09 23:03:33 -0700

    Add editable tilemap
    Left click adds tiles, right click removes them.

--- game/game.go
+++ game/game.go
@@ -4,6 +4,20 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+const (
+	// W is screen width
+	W = 640
+	// H is screen height
+	H = 480
+	// s is size of a tile
+	s = 20
+	// number of tiles in x direction
+	nx = W / s
+	// number of tiles in y direction
+	ny = H / s
 )
 
 // Game implements ebiten.Game interface and stores the game state.
@@ -14,32 +28,55 @@ import (
 //	Draw
 //	Layout
 type Game struct {
-	width  int
-	height int
+	tilemap [][]bool
 }
 
 // NewGame creates a new Game
-func NewGame(width, height int) *Game {
-
-	return &Game{width, height}
+func NewGame() *Game {
+	tilemap := make([][]bool, nx)
+	for i := range tilemap {
+		tilemap[i] = make([]bool, ny)
+	}
+	return &Game{tilemap}
 }
 
 // Update function is called every tick and updates the game's logical state.
 func (g *Game) Update(screen *ebiten.Image) error {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := tilemapIndexOfCursor()
+		g.tilemap[x][y] = true
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		x, y := tilemapIndexOfCursor()
+		g.tilemap[x][y] = false
+	}
 	return nil
+}
+
+// Get tilemap x and y indexes of the mouse cursor
+func tilemapIndexOfCursor() (int, int) {
+	mx, my := ebiten.CursorPosition()
+	return mx / s, my / s
 }
 
 // Draw is called every frame. The frame frequency depends on the display's
 // refresh rate, so if the display is 60 Hz, Draw will be called 60 times per
 // second.
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.Black)
+	screen.Clear()
 
+	for x := range g.tilemap {
+		for y := range g.tilemap[x] {
+			if g.tilemap[x][y] {
+				ebitenutil.DrawRect(screen, float64(x*s), float64(y*s), s, s, color.RGBA{100, 100, 100, 255})
+			}
+		}
+	}
 }
 
 // Layout accepts the window size on desktop as the outside size, and return's
 // the game's internal or pixel screen size, which is then scaled up to fit in
 // the outside size. This does more for resizeable windows.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return g.width, g.height
+	return W, H
 }
--- main.go
+++ main.go
@@ -7,21 +7,14 @@ import (
 	"github.com/jlafayette/edges-from-tilemap/game"
 )
 
-var (
-	// Screen width and height indicates how many pixels to draw, not the
-	// window dimensions.
-	screenWidth  = 620
-	screenHeight = 480
-)
-
 func main() {
 
 	// In this test, window size is equal to screen size, so no pixelation
 	// or stretching will occur.
-	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowSize(game.W, game.H)
 
-	ebiten.SetWindowTitle("Ebiten Test")
-	game := game.NewGame(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Edges from Tilemap")
+	game := game.NewGame()
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
